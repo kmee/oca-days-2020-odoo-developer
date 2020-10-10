0237bd0b396f3c34ea998b01d84166f0c76b26c9
Luis Felipe Mileo <mileo@kmee.com.br>
2020-10-10 08:21:25 -0300

    [NEW] Base of the customer_service
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

---
+++ odoo/local-src/customer_service/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 KMEE
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Customer Service',
+    'description': """
+        ACME Customer Service""",
+    'version': '13.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'KMEE',
+    'website': 'www.kmee.com.br',
+    'depends': [
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+}
